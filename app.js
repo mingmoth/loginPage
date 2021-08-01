a85fda012f20552a6ba5102fb2c441db8663ad6a
mingmoth <mingmoth@gmail.com>
2021-08-01 15:06:25 +0800

    feat: add cookie-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const User = require('./models/user')
 
 const mongoose = require('mongoose')
@@ -18,31 +19,51 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(express.urlencoded({ extended: true }))
+// 設定簽章
+app.use(cookieParser('12345678'))
 
 app.get('/', (req, res) => {
-  res.render('login')
-})
+  const { email, password } = req.signedCookies
 
-app.post('/', (req, res) => {
-  const { email, password } = req.body
-  const msg = 'Username or Password is wrong'
-  User.find()
+  if (email === '' || password === '') {
+    res.render('login')
+  }
+  User.find({ email, password })
     .lean()
     .then((users) => {
-      if (email || password) {
-        users.forEach((user) => {
-          let userName = user.firstName
-          if (email === user.email && password === user.password) {
-            res.render('index', { userName })
-          }
-        })
+      if (users.length === 1) {
+        let userName = users[0].firstName
+        res.render('index', { userName })
+      } else {
+        res.render('login')
+      }
+    })
+    .catch(error => console.log(error))
+  
+})
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  const msg = 'Username or Password is invalid'
+  ind({ email, password })
+    .lean()
+    .then((users) => {
+      if (users.length === 1) {
+        res.cookie('email', email, { path: '/', signed: true, maxAge: 600000 });
+        res.cookie('password', password, { path: '/', signed: true, maxAge: 600000 });
+        res.redirect('/')
       } else {
         res.render('login', { msg: msg })
       }
     })
     .catch(error => console.log(error))
+})
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('email', { path: '/' });
+  res.clearCookie('password', { path: '/' });
+  return res.redirect('/')
 })
 
 app.listen(3000, () => {
